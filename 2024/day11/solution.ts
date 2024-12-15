223368e00c1c3ba0c691ddb2394f94e9abc1267e
fesieg <user.email>
2024-12-15 20:24:25 +0100

    day11 part2 done

--- 2024/day11/solution.ts
+++ 2024/day11/solution.ts
@@ -1,44 +1,62 @@
 import { getFileContent } from "../utils/tsutils";
 
-const stones = (await getFileContent("input.txt")).split(" ");
+const stones = (await getFileContent("input.txt")).split(" ").map(Number);
 
-function getChangedStones(stone: string): string[]{
+function getChangedStones(stone: string): number[] | BigInt[]{
   if (stone === "0") {
-    return ["1"];
+    return [1];
   }
 
   const length = stone.length;
 
   if (length % 2 === 0) {
     const mid = length / 2;
-    const firstHalf = leadingZeroesRemoved(stone.substring(0, mid));
-    const secondHalf = leadingZeroesRemoved(stone.substring(mid));
+    const firstHalf = parseInt(leadingZeroesRemoved(stone.substring(0, mid)));
+    const secondHalf = parseInt(leadingZeroesRemoved(stone.substring(mid)));
     return [firstHalf, secondHalf];
   }
 
-  return [String(BigInt(stone) * 2024n)];
+  return [parseInt(stone) * 2024];
 }
 
-function blinkNTimes(inputStones: string[], blinks: number): string[] {
-  // TODO: use map for part 2
-  // loop through stones and insert result of getChangedStones for each stone
-  let loopStones = structuredClone(inputStones)
-  while (blinks > 0){
-    let stoneCache: string[] = [];
-    for (const stone of loopStones){
-      stoneCache.push(...getChangedStones(stone));
+
+function blinkMapNTimes(inputStones: number[], blinks: number): Map<number, number> {
+  let stoneMap = new Map();
+  
+  for (const stone of inputStones) {
+    // initialize map with each input stones as key, and value as amount of that stone present
+    stoneMap.set(stone, inputStones.filter(s => s === stone).length);
+  }
+
+  while (blinks > 0) {
+    // TODO: people have told me a more efficient way would be recursion, so look into that
+    // for each blink, go through all keys in the map
+    // get the changed stones for each key, and increment value in the map for each changed stone
+    // we use a new map to store the new values, so we don't mess up the current map
+    const newMap = new Map();
+    for (const [stone, count] of stoneMap) {
+      let changedStones = getChangedStones(stone.toString());
+      for (const changedStone of changedStones) {
+        let newCount = newMap.get(changedStone) || 0;
+        newMap.set(changedStone, newCount + count);
+      }
     }
 
-    loopStones = stoneCache;
+    stoneMap = newMap;
+
     blinks--;
   }
 
-  return loopStones
+  return stoneMap;
 }
 
-console.log("PART 1: ", blinkNTimes(stones, 25).length);
-console.log("PART 2: ", blinkNTimes(stones, 75).length);
+console.log("PART 1: ", sumArray([...blinkMapNTimes(stones, 25).values()]));
+console.log("PART 2: ", sumArray([...blinkMapNTimes(stones, 75).values()]));
 
 function leadingZeroesRemoved(input: string): string {
   return input.replace(/^0+(?!$)/, '');
+}
+
+function sumArray(array: number[]): number {
+  return array.reduce((acc, curr) => acc + curr, 0);
 }
